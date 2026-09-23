9ab44f83964a0fefe1b5221c4e394ba00da2a57b
Deepak-Leveledge <deepakleveledge@gmail.com>
2026-09-23 12:34:09 +0530

    added configuration to the app folder

---
+++ app/core/config.py
@@ -0,0 +1,13 @@
+import os 
+from dotenv import load_dotenv
+load_dotenv()
+
+class Settings:
+    PROJECT_NAME: str = "FastAPI Project"
+    API_KEY: str = os.getenv("API_KEY")
+    JWT_SECRET_KEY: str = os.getenv("JWT_SECRET_KEY")
+    REDIS_URL: str = os.getenv("REDIS_URL")
+    JWT_ALGORITHM: str = "HS256"
+    MODELS_PATH: str = "app/models/model.pkl"
+
+settings = Settings()
