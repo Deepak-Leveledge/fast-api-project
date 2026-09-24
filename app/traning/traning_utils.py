e5c47b9662abc154112102f12c40dde47627c42b
Deepak-Leveledge <deepakleveledge@gmail.com>
2026-09-24 10:16:56 +0530

    added notebook and pefrom opration like traninng anf preprocessing

---
+++ app/traning/traning_utils.py
@@ -0,0 +1,11 @@
+import os
+
+
+DATA_DIR = 'data'
+DATA_FILE_NAME = 'car-details.csv'
+DATA_FILE_PATH = os.path.join(DATA_DIR, DATA_FILE_NAME)
+
+
+APP_DIR = 'app'
+MODELS_DIR = 'models'
+MODEL_NAME = 'model.joblib'
