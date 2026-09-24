e3c4e7da14f2a704a680f6626a5966721c974290
Deepak-Leveledge <deepakleveledge@gmail.com>
2026-09-24 23:57:28 +0530

    remove details from exception1

--- app/core/exceptions.py
+++ app/core/exceptions.py
@@ -8,7 +8,7 @@ def register_exception_handlers(app: FastAPI):
     async def http_exception_handler(request: Request, exc: Exception):
         return JSONResponse(
             status_code=exc.status_code,
-            content={"detail": exc.detail},
+            content={"detail": str(exc)},   
         )
 
     # @app.exception_handler(Exception)
